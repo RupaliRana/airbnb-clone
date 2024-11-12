22d5759f487b050067888a6e61110a60f192a609
Rupali Rana <riyaisrupali@gmail.com>
2024-11-12 20:39:24 +0530

    Second Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 
 import './App.css'
-
+import { Route,Routes } from 'react-router-dom';
+import IndexPage from "./pages/IndexPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 function App() {
-  
+
 
   return (
-    <div>test</div>
+    <Routes>
+      <Route index element={<IndexPage />} />
+      <Route path={'/login'} element={<LoginPage />} />
+    </Routes>
+    
   )
 }
 
